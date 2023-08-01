75353de9a434c48c14dff13a48291efb09e0384e
Viraj Kaushalye <virajkaush@gmail.com>
2023-08-01 19:49:09 +0530

    fix border

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="bg-black/50 max-h-min h-screen">
-      <div className="h-screen flex flex-col items-center justify-between pt-12 px-4 text-pink-500">
+    <main className="bg-black/30 max-h-min h-screen border-pink-500 border-8">
+      <div className="flex flex-col h-full items-center justify-between py-16 px-4 text-pink-500">
         <div className="text-center">
           <div className="text-5xl">I&apos;m Sorry, Nangi Baba ❤️</div>
           <div className="text-3xl mt-6">
@@ -10,9 +10,9 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="text-center pt-24 pb-4">
+        <div className="text-center">
           <div className="">I hope you find this meaningfull 🤗</div>
-          <div className="pt-24">-- Aiya Baba 😘 --</div>
+          <div className="pt-12">-- Aiya Baba 😘 --</div>
         </div>
       </div>
     </main>
