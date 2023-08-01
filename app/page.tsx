2bd4b6120f70c5e5703f2b5bb3b54c99fe52e697
Viraj Kaushalye <virajkaush@gmail.com>
2023-08-01 19:38:26 +0530

    changed bg, font & font sizes | add emojies

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,21 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="">I&apos;m Sorry, Baby</div>
-      <div className="">
-        I know I overreacted, and I didn&apos;t mean to come across as not
-        caring. If you feel that way, I truly sorry.
+    <main className="bg-black/50 max-h-min h-screen">
+      <div className="h-screen flex flex-col items-center justify-between pt-12 px-4 text-pink-500">
+        <div className="text-center">
+          <div className="text-5xl">I&apos;m Sorry, Nangi Baba ❤️</div>
+          <div className="text-3xl mt-6">
+            I know I have overreacted 😞, and I didn&apos;t mean to come across
+            as not caring. If you feel that way, I&apos;m truly sorry 😥.
+          </div>
+        </div>
+
+        <div className="text-center pt-24 pb-4">
+          <div className="">I hope you find this meaningfull 🤗</div>
+          <div className="pt-24">-- Aiya Baba 😘 --</div>
+        </div>
       </div>
-
-      <hr />
-
-      <div className="">I hope you find this gift meaningfull</div>
-      <div className="">Your Love. Your Heart. Aiya Baba</div>
     </main>
   )
 }
